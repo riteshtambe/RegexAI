08e2146d3ecc429d2d60f30dcdbe36a1f723cfaa
Ritesh  Tambe <89026426+riteshtambe@users.noreply.github.com>
2023-11-01 22:26:11 +0530

    Add files via upload
    Signed-off-by: Ritesh  Tambe <89026426+riteshtambe@users.noreply.github.com>

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+with open('Readme.md', 'r', encoding='utf-8') as file:
+    long_description = file.read()
+
+
+VERSION = '0.0.12'
+DESCRIPTION = 'Tranform regex into regexai'
+LONG_DESCRIPTION = long_description
+
+# Setting up
+setup(
+    name="regexai",
+    version='0.0.12',
+    author="Shreyash Rote and Ritesh Tambe",
+    author_email="shreyashrote321@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['openai'],
+    keywords=['openai', 'regex', 'pattern', 'text_analysis','text_preprocessing'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
