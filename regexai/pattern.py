5661337aac8e98a9dc5ce8429f43f1a6c4150c36
RajAdhikar7 <105103534+RajAdhikar7@users.noreply.github.com>
2023-11-02 15:40:45 +0530

    Update pattern.py
    removed stopwords by using spacy library to decrease the token size.
    
    Signed-off-by: RajAdhikar7 <105103534+RajAdhikar7@users.noreply.github.com>

--- regexai/pattern.py
+++ regexai/pattern.py
@@ -5,7 +5,23 @@ class apikey:
         #self.api_key = api_key
         self.co = None
         self.da= None
+
+    # generating code for the user query by removing stop words from the given input.
     def find_all(self, da, qu):
+        import spacy
+
+        nlp = spacy.load('en_core_web_sm')
+
+        # Lowercase the text
+        da = da.lower()
+
+        doc = nlp(da)
+
+        tokens = [token.text for token in doc if not token.is_stop]
+
+        # input text paragraph after removing stopwords.
+        da=" ".join(tokens)   
+
         query = f"""with reference to this given paragraph- "{da}", Consider below instructions for writing one line regex code for it - (this para is already stored into "da" variable. So, don't use the paragraph into your code and use "da" variable instead and write full single line python regex code for the given query - "{qu}", store the output into "result" variable.)"""
 
         coderr = openai.ChatCompletion.create(model="gpt-3.5-turbo-16k", messages=[
@@ -21,19 +37,14 @@ class apikey:
         self.co = self.co.split(':', 1)[-1].split("\n#\n\n")[0]
 
 
-        # Execute the generated code
-        #result_dict = {}
+       # catching exceptions 
         try:
             import re
             import importlib
             get=self.co.split('\nresult = ')[1]
             re=importlib.import_module("re")
             return eval(get,globals(),{"da":da,"re":re})
-            # exec_globals = result_dict
-            # #self.da=da
-            # exec(self.co, exec_globals, {"da": self.da})
-            # result = result_dict.get('result')
-            # return result
+           
         except Exception as e:
             print("Error:", e)
 
